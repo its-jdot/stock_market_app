0d551f5b65367c77a4a5e6ad808ff12f5fa682cf
Jamshad Arshad <j.arshad1285@gmail.com>
2022-08-11 15:06:52 +0500

    Error Handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,14 @@ class HomeController < ApplicationController
   
   
   def index
+    @api = StockQuote::Stock.new(api_key: "pk_09440222b4794da68056a63c9781ed46")
     
+    if params[:ticker] == ""
+      @nothing = "You Enetred No Symbol !"
+    elsif params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+    end
+  
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root "home#index"
   
   get 'home/about'
+
+  post '/' => 'home#index'
   
 end
